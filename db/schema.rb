1620569b98106a8f5fac25b97ffe6ad89a14cd0f
acartesc <acartescc@gmail.com>
2018-04-12 23:17:15 -0300

    seed first version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402231324) do
+ActiveRecord::Schema.define(version: 20180413014450) do
 
   create_table "base_salaries", force: :cascade do |t|
     t.integer "amount"
@@ -101,13 +101,30 @@ ActiveRecord::Schema.define(version: 20180402231324) do
     t.index ["worker_id"], name: "index_remunerations_on_worker_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "workers", force: :cascade do |t|
     t.string "rut"
     t.string "first_name"
     t.string "middle_name"
     t.string "last_name1"
     t.string "last_name2"
-    t.date "birthdate"
+    t.date "bithdate"
     t.string "gender"
     t.string "email"
     t.string "phone"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+client = Client.create([{name: 'Cliente 1', client_rut: '78.889.344-1'}])
