3a97fe21fec879d244d7102cfb93dac433f167d7
acartesc <acartescc@gmail.com>
2018-04-12 22:56:19 -0300

    devise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
   resources :companies do
     resources :workers
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307231014) do
+ActiveRecord::Schema.define(version: 20180413014450) do
 
   create_table "companies", force: :cascade do |t|
     t.string "taxpayer_id_company"
@@ -19,10 +19,21 @@ ActiveRecord::Schema.define(version: 20180307231014) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "taxpayer_id_companies", force: :cascade do |t|
-    t.string "business_name"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "workers", force: :cascade do |t|
