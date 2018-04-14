4afff9166b766e968e369adff889c7633c6b4448
acartesc <acartescc@gmail.com>
2018-04-14 11:48:12 -0300

    ability, devise and migration repaired

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,8 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-<<<<<<< HEAD
+
       if user.role == 'admin'
-=======
-      if user.admin?
->>>>>>> master
         can :manage, :all
       else
         can :read, :all
